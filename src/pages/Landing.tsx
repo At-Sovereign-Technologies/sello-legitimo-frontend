b3b248a69dc600fd5dc2051a879e8944310eadbe
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-03-31 22:28:56 -0500

    [CREATE]: Organization, terminada.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/NavBar"
 import Hero from "../components/Hero"
 import Features from "../components/Features"
+import Organization from "../components/Organization"
 
 export default function Landing() {
   return (
@@ -8,6 +9,7 @@ export default function Landing() {
       <NavBar />
       <Hero />
       <Features />
+      <Organization />
     </div>
   )
 }
