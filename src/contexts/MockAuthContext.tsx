266b93279cccfb408a0a509c1419d096a89a2be8
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-06 17:04:56 -0500

    fix

--- src/contexts/MockAuthContext.tsx
+++ src/contexts/MockAuthContext.tsx
@@ -1,10 +1,11 @@
-import React, {
-    createContext,
-    useContext,
-    useState,
-    useEffect,
-    ReactNode,
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
 } from "react";
+
+import type { ReactNode } from "react";
 
 export type MockRole =
     | "CANDIDATO"
